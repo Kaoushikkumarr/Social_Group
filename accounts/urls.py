3bd705b17a7e3f9cbb85f0f5645628280f878fc6
Kaoushikkumarr <Kaoushikkumarr@gmail.com>
2019-09-25 18:05:54 +0530

    5th Setup, where webApp Login&Logout is working now

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     auth_views.LoginView.as_view(
     template_name='accounts/login.html'), name='login'),
     url(r'logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    url(r'signup/$', views.SignUp.as_view(), name='sign')
+    url(r'signup/$', views.SignUp.as_view(), name='signup')
 ]
--- simplesocial/views.py
+++ simplesocial/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
+class TestPage(TemplateView):
+    template_name = 'test.html'
+
+class Thankspage(TemplateView):
+    template_name = 'thanks.html'
+
+
 class HomePage(TemplateView):
     template_name = 'index.html'
